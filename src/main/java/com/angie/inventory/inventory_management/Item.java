701e550e22a32937a7b70404183b435535250af5
angie-3 <acastilloruiz244@gmail.com>
2025-04-01 08:55:36 -0400

    Resolve merge conflicts in .gitignore

---
+++ src/main/java/com/angie/inventory/inventory_management/Item.java
@@ -0,0 +1,15 @@
+package com.angie.inventory.inventory_management; //defines the package where it belongs package declaration 
+
+//declaring the Items class
+public class Item { /*the keyword public means this class can be accessed anywhere in the project.
+    the class name Item, following PascalCase naming conventions 
+    */
+    //defining attributes(fields) these fields are private meaning they cannot be accessed directly from outside the class.
+    //this follows the encapsulation principle OOP. Hiding the data and providing controlled access through methods.
+    private String name;
+    private int quantity;
+    private double price;
+
+    //Constructors,Getters, Setters
+    //Constructor is added to initialize objects
+}
